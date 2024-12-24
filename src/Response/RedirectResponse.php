e6bcd4a8e939c6fd472ccdcc63882315f7a4750a
Joey Smith <jsmith@webinertia.net>
2024-12-24 13:28:20 -0600

    Signed-off-by: Joey Smith <jsmith@webinertia.net>

--- src/Response/RedirectResponse.php
+++ src/Response/RedirectResponse.php
@@ -4,16 +4,18 @@ declare(strict_types=1);
 
 namespace Axleus\Htmx\Response;
 
+use Axleus\Htmx\ResponseHeaders as Header;
+
 use Laminas\Diactoros\Response\RedirectResponse as LaminasRedirectResponse;
 
 final class RedirectResponse extends LaminasRedirectResponse
 {
     public function __construct(
         string $uri,
-        int $status = 200,
-
-        array $headers = []
     ) {
+        // htmx does not evaluate headers with 3xx status codes
+        $status = 200;
+        $headers[Header::HX_Redirect->value] = $uri;
         parent::__construct($uri, $status, $headers);
     }
 }
