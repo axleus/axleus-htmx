416a12e360b8c44aec61d9bae3d8474f723d4f93
Joey Smith <jsmith@webinertia.net>
2024-12-24 18:32:05 -0600

    Signed-off-by: Joey Smith <jsmith@webinertia.net>

--- test/Response/RedirectResponseTest.php
+++ test/Response/RedirectResponseTest.php
@@ -2,30 +2,32 @@
 
 declare(strict_types=1);
 
-namespace AxleusTest\Htmx;
+namespace AxleusTest\Htmx\Response;
 
-use PHPUnit\Framework\TestCase;
+use Axleus\Htmx\ResponseHeaders as Header;
 use Axleus\Htmx\Response\RedirectResponse;
+use PHPUnit\Framework\Attributes\CoversClass;
+use PHPUnit\Framework\TestCase;
 
+#[CoversClass((RedirectResponse::class))]
 final class RedirectResponseTest extends TestCase
 {
+    private string $uri = 'https://example.com';
+
     public function testRedirectResponse()
     {
-        $url = 'https://example.com';
-        $response = new RedirectResponse($url);
-
+        $response = new RedirectResponse($this->uri);
         $this->assertInstanceOf(RedirectResponse::class, $response);
-        $this->assertEquals($url, $response->getUrl());
+        $this->assertEquals($this->uri, $response->getHeader(Header::HX_Redirect->value)[0]);
     }
 
     public function testRedirectResponseWithStatusCode()
     {
         $url = 'https://example.com';
-        $statusCode = 302;
+        $statusCode = 200;
         $response = new RedirectResponse($url, $statusCode);
-
         $this->assertInstanceOf(RedirectResponse::class, $response);
-        $this->assertEquals($url, $response->getUrl());
+        $this->assertEquals($url, $response->getHeader(Header::HX_Redirect->value)[0]);
         $this->assertEquals($statusCode, $response->getStatusCode());
     }
 }
